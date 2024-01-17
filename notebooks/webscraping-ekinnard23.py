7b4e88e453bc06f6a949ffc43782063ea8c68776
ekinnard23 <ekinnard@gmail.com>
2024-01-16 21:19:49 -0600

    First

---
+++ notebooks/webscraping-ekinnard23.py
@@ -0,0 +1,9 @@
+import requests
+from IPython.core.display import HTML
+import pandas as pd
+from sqlalchemy import create_engine, text
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+
